09def6b2e36f55c7c8e32be1dc412344ed6b4e09
joram.andrew.k@gmail.com <joram.andrew.k@gmail.com>
2025-08-29 14:12:00 +0300

    email

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -52,8 +52,8 @@ const Footer: React.FC = () => {
             <div className="space-y-3 animate-fade-in-up delay-300">
               <div className="flex items-center space-x-3">
                 <Mail className="w-5 h-5 text-red-500 flex-shrink-0" />
-                <a href="mailto:alphafold@aceuganda.org" className="text-gray-300 hover:text-white transition-colors">
-                  alphafold@aceuganda.org
+                <a href="mailto:alphafoldtraininguganda2025@gmail.com" className="text-gray-300 hover:text-white transition-colors">
+                  alphafoldtraininguganda2025@gmail.com
                 </a>
               </div>
               <div className="flex items-center space-x-3">
